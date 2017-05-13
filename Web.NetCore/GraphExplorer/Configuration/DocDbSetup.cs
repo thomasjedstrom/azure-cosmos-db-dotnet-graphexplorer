161e683b5dc69c4f3c9d07668c0b8cecbbe782d4
Stephen Stchur <sstchur@microsoft.com>
2017-05-12 17:05:46 -0700

    Porting GraphExplorer proj to .NETCore

---
+++ Web.NetCore/GraphExplorer/Configuration/DocDbSetup.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace GraphExplorer.Configuration
+{
+    // public static class DocDbSettings
+    // {
+    //     public static string DatabaseId = dbConfig.Database;
+    //     public static DocumentClient Client;
+
+    //     public static void Init()
+    //     {
+    //         Client = new DocumentClient(new Uri(dbConfig.Endpoint), dbConfig.AuthKey, new ConnectionPolicy { EnableEndpointDiscovery = false });
+    //     }
+    // }
+}
+  
+
+      
